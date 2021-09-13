980c4768bcb3b204ef4ac663b3323a97bd55b542
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-09-13 20:37:58 +0200

    Added styles to HeaderSearch & HeaderRights at Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { Avatar } from '@material-ui/core';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
     return (
@@ -12,11 +14,39 @@ function Header() {
                 />
                 <AccessTimeIcon />
             </HeaderLeft>
+            <HeaderSearch>
+                <SearchIcon />
+                <input placeholder="Search Tommy" />
+            </HeaderSearch>
+            <HeaderRight>
+                <HelpOutlineIcon />
+            </HeaderRight>
         </HeaderContainer>
     )
 }
 
 export default Header;
+
+const HeaderSearch = styled.div`
+    flex: 0.4;
+    opacity: 1;
+    border-radius: 6px;
+    background-color: #421f44;
+    text-align: center;
+    display: flex;
+    padding: 0 50px;
+    color: gray;
+    border: 1px gray solid;
+
+    > input {
+        background-color: transparent;
+        border: none;
+        text-align: center;
+        min-width: 30vw;
+        outline: 0;
+        color: white;
+    }
+`;
 
 const HeaderContainer = styled.div`
     display: flex;
@@ -38,6 +68,17 @@ const HeaderLeft = styled.div`
     > .MuiSvgIcon-root {
         margin-left: auto;
         margin-right: 30px;
+    }
+`;
+
+const HeaderRight = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: flex-end;
+
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 20px;
     }
 `;
 
