adcf5eeca290a50eb1a4e52beae31e6b9cd81fb5
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-09-15 11:21:54 +0200

    Finished SidebarInfo at Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
 
 function Sidebar() {
     return (
@@ -13,6 +14,7 @@ function Sidebar() {
                         Tomasz Stolarczyk
                     </h3>
                 </SidebarInfo>
+                <CreateIcon />
             </SidebarHeader>
         </SidebarContainer>
     )
@@ -23,8 +25,47 @@ export default Sidebar;
 
 const SidebarContainer = styled.div`
     background-color: var(--slack-color);
+    color: white;
+    flex: 0.3;
+    border-top: 1px solid #49274b;
+    max-width: 260px;
+    margin-top: 60px;
 `;
 
-const SidebarHeader = styled.div``;
+const SidebarHeader = styled.div`
+    display: flex;
+    border-bottom: 1px solid #49274b;
+    padding: 13px;
 
-const SidebarInfo = styled.div``;
+    > .MuiSvgIcon-root {
+        padding: 8px;
+        color: #49274b;
+        font-size: 18px;
+        background-color: white;
+        border-radius: 50%;
+    }
+`;
+
+const SidebarInfo = styled.div`
+    flex: 1;
+
+    > h2 {
+        font-size: 15px;
+        font-weight: 900;
+        margin-bottom: 5px;
+    }
+
+    > h3 {
+        display: flex;
+        font-size: 13px;
+        font-weight: 400;
+        align-items: center;
+    }
+
+    > h3 > .MuiSvgIcon-root {
+        font-size: 14px;
+        margin-top: 1px;
+        margin-right: 2px;
+        color: green;
+    }
+`;
