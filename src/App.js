fed8ad9affc2d19a3e98ce22e1e56c3b9486c736
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-09-21 21:16:32 +0200

    Added HeaderLeft and Header Right to Chat

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import Header from './components/Header';
 import styled from 'styled-components';
 import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
             <Sidebar />
             <Switch>
               <Route path="/" exact>
-                {/* Chat */}
+                <Chat />
               </Route>
             </Switch>
           </AppBody>
