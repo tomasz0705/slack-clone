4cccec053926a4dfff7bc032053b03780159265b
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-09-23 21:56:55 +0200

    Finished styling for HeaderLeft & Right in Chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,20 +6,27 @@ import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 function Chat() {
     return (
         <ChatContainer>
-            <Header>
-                <HeaderLeft>
-                    <h4>
-                        <strong>#Room-name</strong>
-                    </h4>
-                    <StarBorderOutlinedIcon />
-                </HeaderLeft>
+            <>
+                <Header>
+                    <HeaderLeft>
+                        <h4>
+                            <strong>#Room-name</strong>
+                            <StarBorderOutlinedIcon />
+                        </h4>
 
-                <HeaderRight>
-                    <p>
-                        <InfoOutlinedIcon /> Details
-                    </p>
-                </HeaderRight>
-            </Header>
+                    </HeaderLeft>
+
+                    <HeaderRight>
+                        <p>
+                            <InfoOutlinedIcon /> Details
+                        </p>
+                    </HeaderRight>
+                </Header>
+
+                <ChatMessages>
+                    {/* List out the messages */}
+                </ChatMessages>
+            </>
         </ChatContainer>
     )
 }
@@ -33,9 +40,39 @@ const Header = styled.div`
     border-bottom: 1px solid lightgray;
 `;
 
-const HeaderLeft = styled.div``;
+const ChatMessages = styled.div`
 
-const HeaderRight = styled.div``;
+`;
+
+const HeaderLeft = styled.div`
+    display: flex;
+    align-items: center;
+
+    > h4 {
+        display: flex;
+        text-transform: lowercase;
+        margin-right: 10px;
+    }
+
+    > h4 > .MuiSvgIcon-root {
+        align-self: center;
+        margin-left: 10px;
+        font-size: 18px;
+    }
+`;
+
+const HeaderRight = styled.div`
+    > p {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+    }
+
+    > p > .MuiSvgIcon-root {
+        margin-right: 5px !important;
+        font-size: 16px;
+    }
+`;
 
 const ChatContainer = styled.div`
     flex: 0.7;
