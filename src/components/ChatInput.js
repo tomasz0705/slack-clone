d34c551289eba38316e668131c8271e45b28bc59
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-09-25 12:43:56 +0200

    Implemented sendMessage function to button & onChange input state

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,16 +1,31 @@
 import { Button } from '@material-ui/core';
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
+import { db } from '../firebase';
+import firebase from "firebase";
 
 function ChatInput({ channelName, channelId }) {
+    const [input, setInput] = useState('');
+
     const sendMessage = (e) => {
         e.preventDefault(); //Prevents refresh
+
+        if(!channelId) {
+            return false;
+        }
+
+        db.collection("rooms").doc(channelId).collection('messages').add({
+            message: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            user: "Tommy Stolarczyk",
+            userImage: "https://avatars.githubusercontent.com/u/73778092?v=4",
+        })
     }
 
     return (
         <ChatInputContainer>
             <form>
-                <input placeholder={`Message #ROOM`} />
+                <input value={input} onChange={e => setInput(e.target.value)} placeholder={`Message #ROOM`} />
                 <Button hidden type="submit" onClick={sendMessage}>
                     SEND
                 </Button>
