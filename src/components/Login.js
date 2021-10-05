761607ec7075a8e80964113ab31855484f4a448e
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-10-05 19:51:36 +0200

    Added signInWithPopup to signIn handle in Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,12 @@
 import { Button } from '@material-ui/core';
 import React from 'react'
 import styled from 'styled-components';
+import { auth, provider } from '../firebase';
 
 function Login() {
     const signIn = (e) => {
         e.preventDefault();
-
+        auth.signInWithPopup(provider).catch((error) => alert(error.message));
     }
     return (
         <LoginContainer>
