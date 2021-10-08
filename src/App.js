65c27b72172b2df4ad764a8e991e447b8536795e
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-10-08 20:45:47 +0200

    Added spinner to app.js from react-spinkit

--- src/App.js
+++ src/App.js
@@ -12,9 +12,26 @@ import Chat from './components/Chat';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from './firebase';
 import Login from './components/Login';
+import Spinner from 'react-spinkit';
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" alt="slack" />
+
+          <Spinner
+          name="ball-spin-fade-loader"
+          color="purple"
+          fadeIn="none"
+          />
+        </AppLoadingContents>
+      </AppLoading>
+    )
+  }
   return (
     <div className="app">
       <Router>
@@ -39,6 +56,24 @@ function App() {
 }
 
 export default App;
+
+const AppLoading = styled.div`
+
+`;
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
 
 const AppBody = styled.div`
   display: flex;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,10 +14,12 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import { useCollection } from "react-firebase-hooks/firestore";
-import { db } from '../firebase';
+import { auth, db } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function Sidebar() {
-    const [channels, loading, error] = useCollection(db.collection("rooms"));
+    const [channels] = useCollection(db.collection("rooms"));
+    const [user] = useAuthState(auth);
 
     return (
         <SidebarContainer>
